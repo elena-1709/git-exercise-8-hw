8c91a66107c2839959543760c0ae30de5fcd504c
Elena <elena1709@yandex.ru>
2022-10-03 23:08:03 +0300

    commit for hw

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает число (N)
+//а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+for (int a = 2; a <= N; a = a + 2)
+{
+    Console.WriteLine($"{a}\t");
+}
